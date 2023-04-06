3c4824dad917b042f381eb59d9bfad638917a968
Iain <Iain@DESKTOP-O9UC60V>
2023-04-05 22:31:36 -0400

    added comment section generator

--- src/CommentGeneration.cpp
+++ src/CommentGeneration.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <stack>
 
 struct Comment 
 {
@@ -11,7 +12,7 @@ struct Comment
     int pid = 0;
     std::string name;
     std::string text;
-    std::string date;
+    uint64_t date;
 };
 
 int GetCommentsForSubject(const std::string& subject, std::vector<Comment>* comments)
@@ -27,7 +28,7 @@ int GetCommentsForSubject(const std::string& subject, std::vector<Comment>* comm
 
         con->setSchema("blog");
 
-        sql::PreparedStatement* query = con->prepareStatement("SELECT comments.id, parent_id, name, text, date FROM comments INNER JOIN subjects ON comments.subject_id = subjects.id WHERE subject = ?");
+        sql::PreparedStatement* query = con->prepareStatement("SELECT comments.id, parent_id, name, text, UNIX_TIMESTAMP(date) FROM comments INNER JOIN subjects ON comments.subject_id = subjects.id WHERE subject = ?");
         query->setString(1, subject.c_str());
 
         sql::ResultSet* result = query->executeQuery();
@@ -39,8 +40,8 @@ int GetCommentsForSubject(const std::string& subject, std::vector<Comment>* comm
             comment.pid = result->getInt(2);
             comment.name = result->getString(3);
             comment.text = result->getString(4);
-            comment.date = result->getString(5);
-
+            comment.date = result->getInt64(5);
+           
             comments->push_back(comment);
         }
     }
@@ -56,7 +57,7 @@ int GetCommentsForSubject(const std::string& subject, std::vector<Comment>* comm
 
 void GenerateComment(std::ostream& out, const Comment& comment)
 {
-	out << "<div class='comment' id='comment-${id}'>"
+	out << "<div class='comment' id='comment-" << comment.id << "'>"
 		"<h3 class='comment-name mark-section'>" << comment.name << "</h3>"
 		"<p class='comment-text'>" << comment.text << "</p>"
 		"<div class='comment-meta'>"
@@ -65,6 +66,50 @@ void GenerateComment(std::ostream& out, const Comment& comment)
 		"</div>"
 	"</div>" << std::endl;
 }
+
+//void GenerateCommentChainInner(std::ostream& out, int pid, std::unordered_map<int, std::vector<const Comment*>> commentsByPID)
+//{
+//}
+
+
+void GenerateCommentChain(std::ostream& out, const Comment* comment, std::unordered_map<int, std::vector<const Comment*>> commentsByPID)
+{
+    out << "<div class='comment-chain' id='comment-chain-" << comment->id << "'>" << std::endl;
+
+    std::stack<const Comment*> q;
+    q.push(comment);
+
+    while (!q.empty())
+    {
+        const Comment* c = q.top();
+        q.pop();
+
+        //printf("%d\n", c->id);
+
+        GenerateComment(out, *c); // root comment
+
+        if (commentsByPID.count(c->id))
+            for (const Comment* child : commentsByPID.at(c->id))
+                q.push(child);
+    }
+
+    out << "</div>" << std::endl;
+}
+
+void GenerateCommentSection(std::ostream& out, std::unordered_map<int, std::vector<const Comment*>> commentsByPID)
+{
+    if (commentsByPID.count(0) == 0) // no comments
+        return;
+
+    for (const Comment* comment : commentsByPID.at(0))
+    {
+        if (commentsByPID.count(comment->id) > 0)                // this is a parent of other comments
+            GenerateCommentChain(out, comment, commentsByPID);
+        else
+            GenerateComment(out, *comment);
+    }
+}
+
 
 int GenerateComments(std::ostream& out, const std::string& subject)
 {
@@ -79,14 +124,16 @@ int GenerateComments(std::ostream& out, const std::string& subject)
     std::unordered_map<int, std::vector<const Comment*>> byParent;
 
     for (const Comment& comment : comments)
-    {
         byParent[comment.pid].push_back(&comment);
-    }
+
+    for (auto& [pid, list] : byParent)
+        std::sort(list.begin(), list.end(), 
+            [](const Comment* a, const Comment* b) { return a->date > b->date; }
+        );
 
     // group comments by parent chains
 
-    for (Comment comment : comments)
-        GenerateComment(out, comment);
+    GenerateCommentSection(out, byParent);
 
 	return 0;
 }
